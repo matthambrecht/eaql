e4a327b6b99fa3c439573e901f2051271fa9729e
Matthew Hambrecht <hambrechtmatt@gmail.com>
2025-07-19 12:20:00 -0400

    initial conditional parser fixes

--- docs/eaql/PARSER.md
+++ docs/eaql/PARSER.md
@@ -6,12 +6,47 @@ All queries start as a query node which can be branched off into 3 different act
 Table searches are parsed into 4 subnodes. The table to be searched, the columns in that table, a filter which uses optional [conditional parsing](#conditional-parsing) to narrow down queries, and a post processor to do things like limit the number of results retrieved. The table and column parsing are pretty self explanatory.
 
 ### Conditional Parsing
-- **Example Expression**: `i > 2 and ((f < 3) or j > 6)`
-- **Example Parsed to AST**: ![](../images/diagrams/ast-conditional-example.jpg)
 - Rules:
-    - If the next keyword is **And**, use the Right Side of the parent **And** node. If the next keyword is **Or**, the Right Side of the parent **And** node becomes `True`, and we use the Right Side of the nearest ancestor **Or** node to continue parsing. Otherwise, assuming the current number of openede parentheses is 0, all unevaluated nodes can have their children defaulted from this point (**Or** = False, **And** = True)
-    - Open parentheses force an **Or** node to open as whatever child we currently are (this can be seen happening in the right side of the oldest **And** node in the diagram). We store the current number of open parentheses to keep track of their changes in order to ensure tree validity (number of open parentheses seen before processing a child node should never be less than after processing a child node) and a decrease indcates that we default our most recent **Or** ancestor using the aforementioned rules.
-
+- Start with an "OR" node at the beginning always
+- When you get a literal token evaluate it to its base expression and that evaluated expression will go on the left side of an "AND" node. Where that "AND" node goes is your next location as determined by future rules.
+- If you run across an "and" keyword, go to the next available location and add an "AND" node:
+```
+       (AND)                      (AND)
+    /        \           ->    /        \
+  (EXPR)   Unvisited        (EXPR)     (AND)
+```
+- If you run across an "or" keyword, close out until an ancestor `"OR" node, and add an "OR" node as the child.
+```
+            (OR)                        (OR)
+          /                          /         \
+        (AND)                      (AND)        (OR)
+      /       \           ->        / \        /
+   (AND)    Unvisited           (AND)  True   Unvisited
+  /    \                        /   \
+(EXPR) Unvisited             (EXPR)  True
+```
+- If you run across opening parentheses add an "OR" node
+```
+          (OR)                       (OR)
+        /     \                     /    \
+    (AND)     Unvisited   ->     (AND)   Unvisited
+    /   \                        /    \
+(EXPR)  Unvisited             (EXPR)  (OR)
+                                    /      \
+                              Unvisited   Unvisited
+```
+- If you run across a closing parentheses, close out ancestor "OR" node. (If next keyword is "and", add an "AND node to your LS and set its LS to your LS and start from that child's RS, same thing happens with OR you just add an "OR" node).
+```
+            (OR)                              (OR)    
+           /    \                            /   \
+        (AND)   Unvisited                 (AND)   Unvisited
+       /    \                            /    \
+    (EXPR)  (OR)              ->      (EXPR)  (OR)
+          /      \                            /   \
+      (AND)   Unvisited                    (AND)    False
+    /      \                              /     \
+(EXPR)    Unvisited                    (EXPR)    True
+```
 ## Table Mutation
 ![](../images/utils/under_construction.png)
 
--- src/eaql/parser/conditional.rs
+++ src/eaql/parser/conditional.rs
@@ -4,36 +4,25 @@ use crate::eaql::{
     tokens::{Token, TokenType}};
 
 #[derive(Debug, PartialEq)]
-pub enum OrChild {
-    Or(Box<OrConditionNode>),
-    And(Box<AndConditionNode>),
-}
-
-#[derive(Debug, PartialEq)]
-pub enum AndChild {
-    Or(Box<OrConditionNode>),
-    Expression(Box<ExpressionNode>),
-}
-
-#[derive(Debug, PartialEq)]
 pub struct ConditionNode {
-    _or_condition: Option<OrConditionNode>,
+    _condition: ConditionChild,
 
     _depth: u16
 }
 
 #[derive(Debug, PartialEq)]
-pub struct OrConditionNode {
-    _ls: OrChild,
-    _rs: Option<OrChild>,
-
-    _depth: u16
+pub enum ConditionChild {
+    Op(Box<OperandNode>),
+    Expr(Box<ExpressionNode>),
+    Bool(Box<BoolNode>)
 }
 
 #[derive(Debug, PartialEq)]
-pub struct AndConditionNode {
-    _ls: AndChild,
-    _rs: Option<AndChild>,
+pub struct OperandNode {
+    _type: String,
+
+    _ls: ConditionChild,
+    _rs: ConditionChild,
 
     _depth: u16
 }
@@ -47,200 +36,60 @@ pub struct ExpressionNode {
     _depth: u16
 }
 
+#[derive(Debug, PartialEq)]
+pub struct BoolNode {
+    _value: bool,
+
+    _depth: u16
+}
+
 impl ConditionNode {
+    pub fn recurse_build(
+        parent_op: String,
+        tokens: &Vec<Token>,
+        idx: &mut usize,
+        depth: u16
+    ) -> Result<ConditionChild, String> {
+        Err("None".to_string())
+    }
+
     pub fn parse(
         tokens: &Vec<Token>,
         idx: &mut usize,
         depth: u16
     ) -> Result<ConditionNode, String> {
-        let mut finished: bool = false;
-        let mut num_paren: i16 = 0;
+        let root: OperandNode = OperandNode {
+            _type: "OR".to_string(),
+            
+            _ls: match ConditionNode::recurse_build(
+                "OR".to_string(),
+                tokens,
+                idx,
+                depth + 1
+            ) {
+                Ok(node) => node,
+                Err(msg) => return Err(msg)
+            },
+            _rs: match ConditionNode::recurse_build(
+                "OR".to_string(),
+                tokens,
+                idx,
+                depth + 1
+            ) {
+                Ok(node) => node,
+                Err(msg) => return Err(msg)
+            },
 
-        let processed: OrConditionNode  = match OrConditionNode::parse(
-            tokens,
-            idx,
-            depth + 1,
-            &mut finished,
-            &mut num_paren) {
-                Ok(child) => {
-                    child
-                },
-                Err(err) => {
-                    return Err(err);
-                }
-            };
+            _depth: depth + 1,
+        };
 
         return Ok(
             ConditionNode {
-                _or_condition: Some(processed),
+                _condition: ConditionChild::Bool(Box::new(
+                    BoolNode { _value: false, _depth: depth + 1 })),
                 _depth: depth
             }
         )
-    }
-}
-
-impl OrConditionNode {
-    pub fn parse(
-        tokens: &Vec<Token>,
-        idx: &mut usize,
-        depth: u16,
-        finished: &mut bool,
-        num_paren: &mut i16,
-    ) -> Result<OrConditionNode, String> {
-        validate_length(tokens, &(*idx + 1), true)?;
-        
-        /* If the first character is a parentheses we start a new OR node, otherwise we start 
-        AND node */
-        let ls = if tokens[*idx].token_type == TokenType::OpenParen {
-            *idx += 1;
-            match OrConditionNode::parse(tokens, idx, depth + 1, finished, num_paren) {
-                Ok(node) => OrChild::Or(Box::new(node)),
-                Err(err) => return Err(err),
-            }
-        } else {
-            match AndConditionNode::parse(tokens, idx, depth + 1, finished, num_paren) {
-                Ok(node) => OrChild::And(Box::new(node)),
-                Err(err) => return Err(err),
-            }
-        };
-
-        /* After we've branched back up we check if we need to go back down because
-        of an open parentheses. */
-        let rs = if !*finished {
-            // We have an open parentheses so it becomes an OrNode
-            if tokens[*idx].token_type == TokenType::OpenParen {
-                *idx += 1;
-                *num_paren += 1;
-
-                match OrConditionNode::parse(tokens, idx, depth + 1, finished, num_paren) {
-                    Ok(node) => Some(
-                        OrChild::Or(Box::new(node))),
-                    Err(err) => return Err(err),
-                }
-            // We have an "and" so we need to move forward and evaluate the right side as an AndNode
-            } else if tokens[*idx].token_type == TokenType::And {
-                *idx += 1;
-
-                match AndConditionNode::parse(tokens, idx, depth + 1, finished, num_paren) {
-                
-                    Ok(node) => Some(
-                        OrChild::And(Box::new(node))),
-                    Err(err) => return Err(err),
-                }
-            // We have an "or" so we need to move forward and evaluate the right side as an OrNode
-            } else if tokens[*idx].token_type == TokenType::Or { 
-                *idx += 1;
-
-                match OrConditionNode::parse(tokens, idx, depth + 1, finished, num_paren) {
-                    Ok(node) => Some(
-                        OrChild::Or(Box::new(node))),
-                    Err(err) => return Err(err),
-                }
-            // We have a closed parentheses, close out our current or block
-            } else if tokens[*idx].token_type == TokenType::CloseParen {
-                *num_paren -= 1;
-                *idx += 1;
-                None
-            // We have an ending token, finish up
-            } else if vec![
-                    TokenType::EoqToken,
-                    TokenType::PostProcessorEntrance].contains(&tokens[*idx].token_type.clone()) {
-                *finished = true;
-                *idx += 1;
-                None
-            // We just have a regular expression from this point, move down to AndNode 
-            } else { 
-                match AndConditionNode::parse(tokens, idx, depth + 1, finished, num_paren) {
-                    Ok(node) => Some(
-                        OrChild::And(Box::new(node))),
-                    Err(err) => return Err(err),
-                }
-            }
-        } else {
-            None
-        };
-
-        Ok(OrConditionNode {
-            _ls: ls,
-            _rs: rs,
-            _depth: depth,
-        })
-    }
-}
-            
-impl AndConditionNode {
-    pub fn parse(
-        tokens: &Vec<Token>,
-        idx: &mut usize,
-        depth: u16,
-        finished: &mut bool,
-        num_paren: &mut i16
-    ) -> Result<AndConditionNode, String> {
-        validate_length(tokens, &(*idx + 1), true)?;
-        
-        // If the first character is a parentheses we start a new OR
-        let ls: AndChild = if tokens[*idx].token_type == TokenType::OpenParen {
-            *idx += 1;
-            match OrConditionNode::parse(tokens, idx, depth + 1, finished, num_paren) {
-                Ok(node) => AndChild::Or(Box::new(node)),
-                Err(err) => return Err(err),
-            }
-        /* Otherwise we have to evaluate from here and let the expression handle possible errors
-        All base expressions will be stored in the LS of an And node */
-        } else {
-            match ExpressionNode::parse(tokens, idx, depth + 1) {
-                Ok(node) => AndChild::Expression(Box::new(node)),
-                Err(err) => return Err(err),
-            }
-        };
-
-        let rs: Option<AndChild> = if !*finished {
-            if tokens[*idx].token_type == TokenType::OpenParen {
-                *idx += 1;
-                *num_paren += 1;
-
-                match OrConditionNode::parse(tokens, idx, depth + 1, finished, num_paren) {
-                    Ok(node) => Some(AndChild::Or(Box::new(node))),
-                    Err(err) => return Err(err),
-                }
-            } else {
-                // If the next KW is 'and', RS just becomes whatever is beyond that
-                if tokens[*idx].token_type == TokenType::And {
-                    *idx += 1;
-
-                    match OrConditionNode::parse(tokens, idx, depth + 1, finished, num_paren) {
-                        Ok(node) => Some(AndChild::Or(Box::new(node))),
-                        Err(err) => return Err(err),
-                    }
-                // Next keyword is 'or' means we move up the eval tree and use the RS of our parent OR
-                } else if tokens[*idx].token_type == TokenType::Or {
-                    *idx += 1;
-                    None
-                } else if vec![
-                    TokenType::EoqToken,
-                    TokenType::PostProcessorEntrance].contains(&tokens[*idx].token_type.clone()
-                // Next keyword is closed parentheses, we need to move up a branch
-                ) {
-                    *finished = true;
-                    None
-                } else if tokens[*idx].token_type == TokenType::CloseParen { 
-                    *idx += 1;
-                    *num_paren -= 1;
-                    None
-                // We need a valid keyword at this point otherwise something is wrong
-                } else {
-                    return Err(valid_until_warning(tokens, idx));
-                }
-            }
-        } else {
-            None
-        };
-
-        Ok(AndConditionNode {
-            _ls: ls,
-            _rs: rs,
-            _depth: depth,
-        })
     }
 }
 
@@ -295,20 +144,12 @@ impl ExpressionNode {
 }
 
 // Display functions
-impl fmt::Display for AndChild {
+impl fmt::Display for ConditionChild {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            AndChild::Or(node) => write!(f, "{node}"),
-            AndChild::Expression(node) => write!(f, "{node}"),
-        }
-    }
-}
-
-impl fmt::Display for OrChild {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            OrChild::And(node) => write!(f, "{node}"),
-            OrChild::Or(node) => write!(f, "{node}"),
+            ConditionChild::Op(node) => write!(f, "{node}"),
+            ConditionChild::Expr(node) => write!(f, "{node}"),
+            ConditionChild::Bool(node) => write!(f, "{node}"),
         }
     }
 }
@@ -320,56 +161,41 @@ impl fmt::Display for ConditionNode {
 "\n{}(Condition){}{}",
             get_tab(self._depth),
             get_tab(self._depth + 1),
-            self._or_condition
-                .as_ref()
-                .map(|v| v as &dyn fmt::Display)
-                .unwrap_or(&format!(
-"{}N/A",
-            get_tab(self._depth + 1)
-            )),
+            self._condition
         )
     }
 }
 
-impl fmt::Display for AndConditionNode {
+impl fmt::Display for OperandNode {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
-"\n{}(AndNode){}{}",
+"\n{}(Operand){}{}",
             get_tab(self._depth),
             self._ls,
-            self._rs
-                .as_ref()
-                .map(|v| v as &dyn fmt::Display)
-                .unwrap_or(&format!("
-{}(True)",
-            get_tab(self._depth + 1))),
+            self._rs,
         )
     }
 }
 
-impl fmt::Display for OrConditionNode {
+impl fmt::Display for BoolNode {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
-"\n{}(OrNode){}{}",
+"\n{}(Bool::{})",
             get_tab(self._depth),
-            self._ls,
-            self._rs
-                .as_ref()
-                .map(|v| v as &dyn fmt::Display)
-                .unwrap_or(&format!("
-{}(False)",
-            get_tab(self._depth + 1))),
+            self._value,
         )
     }
 }
+
+
 
 impl fmt::Display for ExpressionNode {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
-"\n{}(ExpressionNode)
+"\n{}(Expression)
 {}variable: {:?}
 {}operator: {:?}
 {}value: {:?}",
