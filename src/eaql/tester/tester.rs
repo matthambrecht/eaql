7f73bba73fc3aaf5a56f7ab55a1098a4b8222423
Matthew Hambrecht <hambrechtmatt@gmail.com>
2025-07-26 23:03:34 -0400

    abstracted query_stdin

--- src/eaql/tester/tester.rs
+++ src/eaql/tester/tester.rs
@@ -1,7 +1,7 @@
-use std::io::{self, Write};
 use crate::{
     utils::{
-        logger
+        logger,
+        io
     },
     eaql::{
         eaql::process_query
@@ -9,17 +9,11 @@ use crate::{
 };
 
 pub fn run() {
-    // Should probably begin to abstract this functionality out, will need to be used elsewhere soon
     loop {
         // Get input
-        let mut line: String = String::new();
-        print!(">>> ");
-        io::stdout().flush().unwrap();
-        io::stdin().read_line(&mut line).unwrap();
-        line = line.trim().to_string(); 
-        logger::debug(&format!("Received Query String -> \"{}\"", line));
+        let query: String = io::query_stdin();
 
-        match process_query(&line) {
+        match process_query(&query) {
             Some(_) => logger::info("Valid query!"),
             None => logger::warning("Invalid query, see above warnings for issues!")
         };
